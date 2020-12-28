afef4c40a30aa995e632d85645f952f4d8d413e2
Alex <gathurialex4@gmail.com>
2020-12-28 15:32:38 +0300

    Added jquery within my index file

---
+++ Java/script.js
@@ -0,0 +1,12 @@
+function icon(){
+var name = document.getElementById("icon11").innerHTML="My name is alex"
+
+if (name.src.match ="../week3/Static/assets/services_icons/design_icon.png") {
+    name = ("My name is alex")  
+
+} else {
+    name.src.match ="../week3/Static/assets/services_icons/design_icon.png";
+
+}
+
+}
