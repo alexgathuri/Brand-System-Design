43db2d133076b605baceebb579c385b6da56761a
Alex <gathurialex4@gmail.com>
2021-01-03 09:54:57 +0300

    Added the hide and show in jqueary

--- Java/script.js
+++ Java/script.js
@@ -1,15 +1,20 @@
-function icon(){
- document.getElementById("icon11").innerHTML="<b>DESIGN </b> <br> <br>Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. <br><br> Throughout you project, our desingers create and implement visual design and  workflows, solicit user feedback and work with you to make sure what gets built is what is needed. " 
+$(document).ready(function() {
+    $(".click").click(function() {
+      $(".show").toggle();
+      $(".hide").toggle();
+    });
+  });
 
+  $(document).ready(function() {
+    $(".click2").click(function() {
+      $(".show2").toggle();
+      $(".hide2").toggle();
+    });
+  });
 
-}
-
-function icon2(){
-    document.getElementById("icon22").innerHTML="<b>DEVELOPMENT </b> <br> <br> All engineers are fluent in the latest enterprise, mobile and web development technologies. <br><br> They  collaborate with your team to write and improve code on a daily basis, using proven practices such as test-driven development and pair programming.  " 
-    
-} 
-
-function icon3(){
-    document.getElementById("icon33").innerHTML="<b>PRODUCT MANAGEMENT </b> <br> <br> Planning and development isiterative. Because we are constantly coding and testing, the products we build are always ready to go live <br><br> This iterative proccess allowsfor changes as business requirements evolve. " 
-    
-} 
+  $(document).ready(function() {
+    $(".click3").click(function() {
+      $(".show3").toggle();
+      $(".hide3").toggle();
+    });
+  });
